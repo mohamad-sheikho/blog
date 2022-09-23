b6542a6acbe476f318b1157d79bca0be2541f32f
dark sassuke <mohamad.sheikho@laplateforme.io>
2022-09-23 23:24:48 +0200

    Dashboard admin terminé
    maque juste de pouvoir ajouter des utilisateurs

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->setEntityLabelInPlural('Utilisateurs')
+        ->setEntityLabelInSingular('utilisateur')
+
+            ->setPageTitle("index", "Blog - Administration des utilisateurs")
+            ->setPaginatorPageSize(10);
+        
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+            ->hideOnForm(),
+            TextField::new('username')
+                ->hideOnForm(),
+            ArrayField::new('roles')
+            ->hideOnIndex(),
+
+        ];
+    }
+
+}
